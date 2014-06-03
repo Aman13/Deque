6216c6691ca9f8a5c6a9879d903cfe7563506866
Aman13 <sandhu.amandeep.s@gmail.com>
2014-06-02 17:30:13 -0700

    Intial testing for template DEQUE, segmentation fault from dq2(dq1)

--- Deque.h
+++ Deque.h
@@ -1,48 +1,154 @@
-
 #include <iostream>
 #include <stdexcept>
 
+template <class T>
 class Deque	{
 
 public:
 
-	Deque (void);
+	Deque () : front(NULL), back(NULL) { }
 
-	Deque(const Deque & source);
+	Deque(const Deque & source)	{
+		Deque();
+		std::cout << "deepy copy" << std::endl;
+		deepCopy(source);
+	}
 
-	~Deque(void);
+	~Deque()	{
+		deleteDeque();
+	}
 
-	Deque & operator= (const Deque & source);
+	Deque & operator= (const Deque & source)	{
+		std::cout << "entering overloaded operator" << std::endl;
+		if(this != &source)	{
+			deleteDeque();
+			deepCopy(source);
+		}
+		return *this;
+	}
 
-	void insert_front(int value);
+	void insert_front(T value)		{
+		if(this->empty())	{
+			 this->front = this->back = new Node(value, this->front);
+		}
+		else	{
+			this->front = new Node(value, this->front);
+		}
+	}
 
-	void insert_back(int value);
+	void insert_back(T value)	{
+		if(this->empty())	{
+			this->front = this->back = new Node(value, this->back);
+		}
+		else	{
+			Node* temp = this->back;
+			this->back = new Node(value, temp->next);
+			temp->next = this->back;
+		}
+	}
 
-	int remove_front();
+	T remove_front()	{
+		this->gaurd();
+		Node* temp = this->front;
+		this->front = front->next;
+		T storage  = temp->data;
+		delete temp;
+		return storage;
+	}
 
-	int remove_back();
+	T remove_back()	{
+		this->gaurd();
+		Node* temp = this->front;
+		if(this->front == this->back)	{
+			this->front = this->back = NULL;
+			T storage = temp->data;
+			delete temp;
+			return storage;
+		}
+		while(temp->next != this->back)	{
+			temp = temp->next;
+		}
+		this->back = temp;
+		temp = temp->next;
+		back->next = NULL;
+		T storage = temp->data;
+		delete temp;
+		return storage;
+	}
 
-	int peek_front()	const;
+	T peek_front()	const	{
+		this->gaurd();
+		return front->data;
+	}
 
-	int peek_back()	const;
+	T peek_back()	const	{
+	this->gaurd();
+	return back->data;
+	}
 
-	bool empty()	const;
+	bool empty()	const	{
+		return this->front == NULL && this->back == NULL;
+	}
 
-	int size()	const;
+	int size()	const	{
+		int counter = 0;
+		Node* temp = this->front;
+		while(temp != NULL)	{
+			++counter;
+			temp = temp->next;
+		}
+		return counter;
+	}
 
 private:
 
 	class Node	{
 	public:
-		int data;
+		T data;
 		Node* next;
 
-		Node(int value): data(value), next(NULL) {};
-		Node(int value, Node* nd): data(value), next(nd) {};
+		Node(T value): data(value), next(NULL) {};
+		Node(T value, Node* nd): data(value), next(nd) {};
 	};
 
 	Node* front;
 	Node* back;
-	void deepCopy(const Deque & ls);
-	void deleteDeque();
+
+	void gaurd()	const	{
+		if(this->empty())
+			throw std::runtime_error("Deque is empty.");
+	}
+
+	void deepCopy(const Deque & source)	{
+		std::cout << "entering void deepCopy" << std::endl;
+		if(source.front == NULL)	{
+			std::cout << "source front = NULL" << std::endl;
+			this->front = NULL;
+			this->back = NULL;
+		}
+		else	{
+			std::cout << "source front = stuff" << std::endl;
+			Node* temp = source.front;
+			std::cout << "node temp is set, entering while loop" << std::endl;
+			int x = 0;
+			while(temp != NULL)	{
+				std::cout <<"looping around: " << x << std::endl;
+				++x;
+				this->insert_back(temp->data);
+				std::cout <<" leaving inser_back" << std::endl;
+				temp = temp->next;
+			}
+			delete temp;
+			std::cout<< "leaving while loop" << std::endl;
+		}
+	}
+	void deleteDeque()	{
+		Node* temp = this->front;
+		while(temp != NULL)	{
+			temp = front->next;
+			delete front;
+			front = temp;
+		}
+		this->front = this->back = NULL;
+	}
 };
--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,7 @@
 
 #include "Deque.h"
 #define assert(x, msg) if (!(x)) throw msg;
-
+/*
 void test_size()	{
 	Deque test;
 	test.insert_front(1);
@@ -65,4 +65,30 @@ int main()	{
 		return 1;
 	}
 	return 0;
+}*/
+int main()	{
+	Deque<int> dq1;
+    std::cout << dq1.empty() << " - 1" << std::endl;
+
+    dq1.insert_front(42);
+    dq1.insert_back(216);
+ 
+    std::cout << dq1.peek_front() << " - 42" << std::endl;
+    std::cout << dq1.peek_back() << " - 216" << std::endl;
+    std::cout << dq1.size() << " - 2" << std::endl;
+ 
+    Deque<int> dq2(dq1);
+    Deque<int> dq3;
+    dq3 = dq1;
+ 
+    std::cout << dq1.remove_front() << " - 42" << std::endl;
+    std::cout << dq1.remove_back() << " - 216" << std::endl;
+ 
+    std::cout << dq2.peek_front() << " - 42" << std::endl;
+    std::cout << dq2.peek_back() << " - 216" << std::endl;
+ 
+    std::cout << dq3.peek_front() << " - 42" << std::endl;
+    std::cout << dq3.peek_back() << " - 216" << std::endl;
+ 
+    return 0;
 }
